b2362b4e0c53a99d360e3aee31ca00d48bfdc16a
daniel.seeff@gmail.com <daniel.seeff@gmail.com>
2013-11-03 14:17:48 -0800

    Added the testimoneals with fading

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ $(window).load(function() {
     animation: "slide"
   });
 });
+
+
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.a-testimonial');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
+};  
+
+fade($('#testimonials > .a-testimonial').first());
